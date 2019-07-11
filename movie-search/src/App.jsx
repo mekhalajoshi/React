cb8e6f5393a29a94f54b05452f52ef531da868ba
Mekhala Joshi <mekhalajoshi@gmail.com>
2019-07-11 13:36:36 -0400

    new commit

--- movie-search/src/App.jsx
+++ movie-search/src/App.jsx
@@ -1,18 +1,11 @@
 import React, { Component } from 'react'
-<<<<<<< Updated upstream
 import './App.css';
-=======
->>>>>>> Stashed changes
 import MovieSearchApp from './components/MovieSearchApp';
 
 class App extends Component {
   render() {
     return (
       <div >
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
         <MovieSearchApp />
       </div>
     );
