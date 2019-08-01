42e6932a4ed74811d71b1255cc0d1bc2f43f1d41
Mekhala Joshi <mekhalajoshi@gmail.com>
2019-08-01 09:50:28 -0400

    Fixed UI

--- movie-search/src/components/SearchBar.jsx
+++ movie-search/src/components/SearchBar.jsx
@@ -3,57 +3,56 @@ import Paper from '@material-ui/core/Paper'
 import InputBase from '@material-ui/core/InputBase'
 import IconButton from '@material-ui/core/IconButton'
 import SearchIcon from '@material-ui/icons/Search'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 const styles = {
-  Paper: {
-
-  },
-  InputBase: {
-    width: '92%',
-    padding: '5px',
-    margin: 'auto',
-    flexGrow: 1
-  }
+	Paper: {
+		margin: '20px',
+		padding: '10',
+		alignItems: 'center',
+		display: 'flex'
+	},
+	InputBase: {
+		marginLeft: 8,
+		flex: 1
+	}
 }
 
 export default class SearchBar extends Component {
-  constructor (props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-    this.onTextChange = this.onTextChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-  }
+		this.onTextChange = this.onTextChange.bind(this)
+		this.onSubmit = this.onSubmit.bind(this)
+	}
 
-  onTextChange = e => {
-    this.props.onChange(e.target.value)
-  };
+	onTextChange = e => {
+		this.props.onChange(e.target.value)
+	}
 
-  onSubmit = () => {
-    this.props.onSubmit()
-  };
+	onSubmit = () => {
+		this.props.onSubmit()
+	}
 
-  render () {
-    return (
-      <div>
-        <Paper
-          style={styles.Paper}
-        >
-          <InputBase
-            style={styles.InputBase}
-            placeholder="Movie Search App"
-            inputProps={{ 'aria-label': 'Movie Search App' }}
-            onChange={this.onTextChange}
-          />
-          <IconButton onClick={this.onSubmit} >
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Paper style={styles.Paper}>
+					<InputBase
+						style={styles.InputBase}
+						placeholder="Movie Search App"
+						inputProps={{ 'aria-label': 'Movie Search App' }}
+						onChange={this.onTextChange}
+					/>
+					<IconButton onClick={this.onSubmit}>
+						<SearchIcon />
+					</IconButton>
+				</Paper>
+			</div>
+		)
+	}
 }
 SearchBar.propTypes = {
-  onChange: PropTypes.func,
-  onSubmit: PropTypes.func,
-};
+	onChange: PropTypes.func,
+	onSubmit: PropTypes.func
+}
